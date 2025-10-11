71ff28e0ed4da00db50a13dfbed957551e663ce0
Janaka Abeywardhana <contact@janaka.co.uk>
2025-10-11 22:03:11 +0100

    chore: update easyrunner-cli@beta to 0.0.6b0
    Beta release from main branch

--- Formula/easyrunner-cli@beta.rb
+++ Formula/easyrunner-cli@beta.rb
@@ -3,9 +3,9 @@ class EasyrunnerCliATBeta < Formula
 
   desc "Beta builds of EasyRunner CLI - Single server self-hosting PaaS"
   homepage "https://github.com/janaka/easyrunner"
-  url "https://github.com/janaka/easyrunner/releases/download/v0.0.5b0/easyrunner_cli-0.0.5b0-py3-none-any.whl"
-  sha256 "045ebd93ac5479f827240136b56c81900ee496cdf3d3f106797aec4f7bdd8d3d"
-  version "0.0.5b0"
+  url "https://github.com/janaka/easyrunner/releases/download/v0.0.6b0/easyrunner_cli-0.0.6b0-py3-none-any.whl"
+  sha256 "144eefd3361f6d0aa8ff047bc2be98d9546fbe1e08db5f5925333f995df5a07f"
+  version "0.0.6b0"
 
   depends_on "python@3.11"
 
