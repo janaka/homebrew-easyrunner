fd5c37e63cbf644cbde237d60e649edc17bafd20
Janaka A <contact@janaka.co.uk>
2026-06-03 20:05:52 +0100

    fix: update easyrunner-cli-beta formula to python@3.13

--- Formula/easyrunner-cli-beta.rb
+++ Formula/easyrunner-cli-beta.rb
@@ -4,11 +4,11 @@ class EasyrunnerCliBeta < Formula
   url "https://files.pythonhosted.org/packages/26/a1/19d14989d22a854a9a53e334de58d262cff4ea77cdd569f4e0cf279743e4/easyrunner_cli-0.40.0b1-py3-none-any.whl"
   sha256 "64956f98e49b1bc11748168ff23ea80289b75ea40216683ffe0b32d3a455f934"
 
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   def install
     # Create a virtual environment and install the package with all dependencies
-    python_version = "3.12"
+    python_version = "3.13"
     python = Formula["python@#{python_version}"].opt_bin/"python#{python_version}"
     venv = libexec/"venv"
     system python, "-m", "venv", venv
